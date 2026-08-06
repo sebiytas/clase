9649dfa32c9bd32a106f0e8bbfb11abb4f24b1d4
sebiytas <sebastiandavidalvarez2010@gmail.com>
2026-08-06 11:13:09 -0400

    Add files via upload

--- tarea modulo 1/2.py
+++ tarea modulo 1/2.py
@@ -1,21 +1,22 @@
-secreto=24
-respuesta= "si"
+import random
+jugar = "si"
 
-while respuesta == "si" :  
+while jugar == "si" :
+    secreto = random.randint(1,10)
     for i in range(0, 3):
-        respuesta= int(input(f"adivina el numero (tienes {3-i} intentos) "))
-        if i <3:
-            if respuesta < secreto:
-                print("intento fallido. prueba con un numero mayor")
-            
-            elif respuesta >secreto:
-                print("intento fallido. prueba con un numero menor")
-            
-            else:
+        respuesta= int(input(f"adivina el numero entre el 1 y el 10 (tienes {3-i} intentos) "))
+        if i == 2:
+            print("te quedaste sin intentos. PERDISTE") 
+        elif i <3:
+            if respuesta == secreto:
                 print(f"adivinaste, el numero es {secreto}")
-                break           
+                break
+            elif respuesta >secreto:
+                print("intento fallido. prueba con un numero menor")           
+            else: 
+                print("intento fallido. prueba con un numero mayor")
         i +=1
-        print("te quedaste sin intentos. PERDISTE")
-    respuesta = input("quiere volver a jugar? ")
+        
+    jugar = input("quiere volver a jugar? ").lower()
     
             
